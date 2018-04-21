186d166235c5ac74a677586ab34f64ce5a3c4941
wellysson <wellyssonam@gmail.com>
2018-04-21 15:16:41 -0300

    tarefa 2 - atualização do rg do cliente através da função atualizar_cliente_rg e criação das funções waiting_message_return, info_buscando_cliente

--- odoo.py
+++ odoo.py
@@ -9,12 +9,14 @@ class connect_odoo(object):
     def __init__(self, dic_info):
         self.dic_info = dic_info
         self.uid = self.get_uid_user()
+        self.id_last_client = False
         self.model = self.init_call_method()
         self.menu()
 
     def menu(self):
         print "====== ODOO TEST ======"
         print "1 - Inserir cliente"
+        print "2 - Atualizar RG cliente recém cadastrado pelo programa"
         print "0 - Sair"
         option = raw_input("Opção: ")
         
@@ -28,6 +30,14 @@ class connect_odoo(object):
             telefone = raw_input("Telefone: ")
             cep = raw_input("CEP: ")
             id_client = self.inserir_cliente(nome, email, telefone, cep)
+        elif option == "2":
+            print ">>>> 2 - Atualizar RG cliente recém cadastrado pelo programa"
+            if self.id_last_client is False:
+                print "Depois que o programa iniciou nenhum cliente foi cadastrado!"
+                self.waiting_message_return()
+            else:          
+                self.atualizar_cliente_rg()
+            
         else:
             os.system('cls' if os.name == 'nt' else 'clear')
             self.menu()
@@ -42,6 +52,12 @@ class connect_odoo(object):
         uid_user = info.authenticate(self.dic_info["db"], self.dic_info["username"], self.dic_info["password"], {})
         return uid_user
 
+    def info_buscando_cliente(self, id_client):
+        model_name = "res.partner"
+        info = self.model.execute_kw(self.dic_info["db"], self.uid, self.dic_info["password"], model_name, "search_read",
+            [[["id", "=", id_client]]], {"fields": ["name", "email", "phone", "zip", "rg_fisica"], "limit": 1})
+        return info
+
     def init_call_method(self):
         """ call method """
         model = xmlrpclib.ServerProxy(self.dic_info["url"] + '/xmlrpc/2/object')
@@ -54,14 +70,32 @@ class connect_odoo(object):
         try:
             id_client = self.model.execute_kw(self.dic_info["db"], self.uid, self.dic_info["password"], model_name, "create", [dic])
             if id_client != False:
+                self.id_last_client = id_client
                 print "Cliente cadastrado com sucesso!"
         except:
             print "Não foi possível cadastrar cliente!"
         
-        print "em segundos você retornará ao menu principal ....."
-        time.sleep(4)
+        self.waiting_message_return()
         return id_client
 
+    def atualizar_cliente_rg(self):
+        # 2 - Atualizar cadastro do cliente recém criado para adicionar o RG do mesmo.
+        rg_number = raw_input("Digite RG: ")
+        model_name = "res.partner"
+        dic = {"rg_fisica": rg_number}
+        try:
+            self.model.execute_kw(self.dic_info["db"], self.uid, self.dic_info["password"], model_name, "write", [[self.id_last_client], dic])
+            info_client = self.info_buscando_cliente(self.id_last_client)
+            if info_client[0]["rg_fisica"].__contains__(rg_number):
+                print "Cadastro atualizado com sucesso!"
+        except:
+            print "Não foi possível atualizar cadastro do cliente!"
+        
+        self.waiting_message_return()
+
+    def waiting_message_return(self):
+        print "clique em [enter] para retornar ao menu principal ..."
+        raw_input()
 
 
 dic_info = {
